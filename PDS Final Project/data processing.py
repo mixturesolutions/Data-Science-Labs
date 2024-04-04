ff6c467dc9c2d2f7c92b7865985db99916ed94dd
“mixturesolutions” <tuo16213@ttemple.edu>
2024-04-04 15:59:27 -0400

    added final project dataset

---
+++ PDS Final Project/data processing.py
@@ -0,0 +1,45 @@
+import torch
+import torchvision
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from tqdm import tqdm  # Import tqdm for progress bar
+
+# downloaded path to the pre-trained ResNet-50 weights (download using *wget -c --no-check-certificate https://download.pytorch.org/models/resnet152-b121ed2d.pth*)
+weights_path = 'resnet50-19c8e357.pth'
+
+# load pre-trained ResNet-50 model without loading pre-trained weights
+model = torchvision.models.resnet50(pretrained=False)
+
+# load pre-trained weights using load_state_dict
+model.load_state_dict(torch.load(weights_path))
+
+# remove the fully connected layer
+model = torch.nn.Sequential(*list(model.children())[:-1])
+model.eval()
+
+# define data transformations
+data_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# load bird image dataset
+dataset = ImageFolder(root='/Users/mixturesolution/Desktop/PDS Final Project/train', transform=data_transforms)  # Update with your dataset path
+dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
+
+# extract features from images
+features = []
+with torch.no_grad():
+    # use tqdm to create a progress bar because it takes forever to 
+    for images, _ in tqdm(dataloader, desc="Extracting Features", unit="batch", leave=False):
+        # Extract features from the second-to-last layer
+        outputs = model(images)
+        features.append(outputs.squeeze())
+
+# Stack features along a new dimension
+features = torch.stack(features)
+
+# Save extracted features to disk
+torch.save(features, 'Features_Extracted.pt')
